3f91ddd8bf18d5c5a566175ca99078ab71740c08
Shiva Kishore Tekale <shivakishore@codeaurora.org>
2016-09-15 17:29:35 +0530

    Telephony: Use data and voice states for SPN
    SPN display text is based on both voice service state
    and data service state.
    Eg - Voice service state is OOS and data service state
    is IN_SERVICE. In such cases, both regStates need to be
    considered to determine what to show in UI for SPN.
    Fix the wrong CDMA carrier text in expanded status bar.
    After updating operator, the expanded status bar will
    update SPN
    For GSM, SPN is "no service" if service is out of service.
    For CDMA, SPN is CDMA carrier. Check current service
    state whether out of service, if yes, display "no service"
    instead of SPN.
    CRs-Fixed: 711942, 532471
    
    Change-Id: Ic46ac0c7e3af51a380bb2db5d71a71bf9595c2ec

--- src/java/com/android/internal/telephony/ServiceStateTracker.java
+++ src/java/com/android/internal/telephony/ServiceStateTracker.java
@@ -2175,8 +2175,9 @@ public class ServiceStateTracker extends Handler {
             String plmn = null;
             boolean showPlmn = false;
             int rule = (iccRecords != null) ? iccRecords.getDisplayRule(mSS.getOperatorNumeric()) : 0;
-            if (mSS.getVoiceRegState() == ServiceState.STATE_OUT_OF_SERVICE
-                    || mSS.getVoiceRegState() == ServiceState.STATE_EMERGENCY_ONLY) {
+            int combinedRegState = getCombinedRegState();
+            if (combinedRegState == ServiceState.STATE_OUT_OF_SERVICE
+                    || combinedRegState == ServiceState.STATE_EMERGENCY_ONLY) {
                 showPlmn = true;
                 if (mEmergencyOnly) {
                     // No service but emergency call allowed
@@ -2189,7 +2190,7 @@ public class ServiceStateTracker extends Handler {
                 }
                 if (DBG) log("updateSpnDisplay: radio is on but out " +
                         "of service, set plmn='" + plmn + "'");
-            } else if (mSS.getVoiceRegState() == ServiceState.STATE_IN_SERVICE) {
+            } else if (combinedRegState == ServiceState.STATE_IN_SERVICE) {
                 // In either home or roaming service
                 plmn = mSS.getOperatorAlpha();
                 showPlmn = !TextUtils.isEmpty(plmn) &&
@@ -2295,6 +2296,15 @@ public class ServiceStateTracker extends Handler {
                         "state is off");
                 plmn = null;
             }
+
+            int combinedRegState = getCombinedRegState();
+            if (combinedRegState == ServiceState.STATE_OUT_OF_SERVICE) {
+                plmn = Resources.getSystem().getText(com.android.internal.
+                    R.string.lockscreen_carrier_default).toString();
+                if (DBG) log("updateSpnDisplay: radio is on but out " +
+                    "of service, set plmn='" + plmn + "'");
+            }
+
 
             if (mSubId != subId || !TextUtils.equals(plmn, mCurPlmn)) {
                 // Allow A blank plmn, "" to set showPlmn to true. Previously, we
@@ -5071,7 +5081,21 @@ public class ServiceStateTracker extends Handler {
         return isInNetwork(b, network, CarrierConfigManager.KEY_CDMA_NONROAMING_NETWORKS_STRING_ARRAY);
     }
 
-    /** Check if the device is shutting down. */
+   /**
+     * Consider dataRegState if voiceRegState is OOS to determine SPN to be displayed
+     */
+    protected int getCombinedRegState() {
+        int regState = mSS.getVoiceRegState();
+        int dataRegState = mSS.getDataRegState();
+        if ((regState == ServiceState.STATE_OUT_OF_SERVICE)
+                && (dataRegState == ServiceState.STATE_IN_SERVICE)) {
+                    log("getCombinedRegState: return STATE_IN_SERVICE as Data is in service");
+                     regState = dataRegState;
+        }
+        return regState;
+    }
+
+  /** Check if the device is shutting down. */
     public boolean isDeviceShuttingDown() {
         return mDeviceShuttingDown;
     }
