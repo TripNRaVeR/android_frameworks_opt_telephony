d5915475e6b55753b2f3a2d29642c8a67d5fa280
Jack Yu <jackyu@google.com>
2017-05-11 13:47:56 -0700

    Fixed that APN context stuck in SCANNING state
    1. When data retry fails at trySetupData, we should apply
       FAILED state to the APN context otherwise it will be
       in SCANNING state forever since we are not going to
       schedule for the next retry.
    2. When we get a new network request in applyNewState,
       if the APN is in SCANNING state, we should trySetupData
       immediately instead of waiting for the next retry,
       which might still minutes/hours away from now.
    
    Test: Telephony sanity tests
    bug: 38113800
    Merged-In: I6edd7555327f41ee713a256edfe641a41ca6f969
    Change-Id: I6edd7555327f41ee713a256edfe641a41ca6f969

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -1643,9 +1643,15 @@ public class DcTracker extends Handler {
                 str.append("emergency = true");
             }
 
+            // If this is a data retry, we should set the APN state to FAILED so it won't stay
+            // in SCANNING forever.
+            if (apnContext.getState() == DctConstants.State.SCANNING) {
+                apnContext.setState(DctConstants.State.FAILED);
+                str.append(" Stop retrying.");
+            }
+
             if (DBG) log(str.toString());
             apnContext.requestLog(str.toString());
-
             return false;
         }
     }
@@ -2507,7 +2513,6 @@ public class DcTracker extends Handler {
                 DctConstants.State state = apnContext.getState();
                 switch(state) {
                     case CONNECTING:
-                    case SCANNING:
                     case CONNECTED:
                     case DISCONNECTING:
                         // We're "READY" and active so just return
@@ -2517,6 +2522,7 @@ public class DcTracker extends Handler {
                     case IDLE:
                         // fall through: this is unexpected but if it happens cleanup and try setup
                     case FAILED:
+                    case SCANNING:
                     case RETRYING: {
                         // We're "READY" but not active so disconnect (cleanup = true) and
                         // connect (trySetup = true) to be sure we retry the connection.
